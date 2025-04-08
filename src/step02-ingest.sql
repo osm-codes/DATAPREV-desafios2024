56a025cf3012cb39b21605e596a8d5cd24e0f7a7
flipreis21 <lipe.reis.21@gmail.com>
2025-04-08 17:27:25 -0300

    Update step02-ingest.sql
    Troca de diretório para ingestão

--- src/step02-ingest.sql
+++ src/step02-ingest.sql
@@ -8,7 +8,7 @@
 DO $do$
 DECLARE
  i text;
- ibge_files text DEFAULT '/tmp/IBGE/'; -- please CHANGE HERE to your path!!
+ ibge_files text DEFAULT '/mnt/dados/IBGE/'; -- please CHANGE HERE to your path!!
 BEGIN
    FOREACH i IN ARRAY '{11_RO,12_AC,13_AM,14_RR,15_PA,16_AP,17_TO,21_MA,22_PI,23_CE,24_RN,25_PB,26_PE,27_AL,28_SE,29_BA,31_MG,32_ES,33_RJ,35_SP,41_PR,42_SC,43_RS,50_MS,51_MT,52_GO,53_DF}'::text[] LOOP
       CALL dpvd24.ins_on_t01_ibge_cnefe2022_point(ibge_files|| i ||'.csv');
